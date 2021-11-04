6710ae832e0b6b58a527acdd54d3b8d18950b52f
sergio974 <serge.larisson@gmail.com>
2021-11-04 15:51:09 +0100

    v0.01 : update 1

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import { useRoutes } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core';
@@ -6,14 +7,79 @@ import 'src/mixins/chartjs';
 import theme from 'src/theme';
 import routes from 'src/routes';
 
+import Amplify from 'aws-amplify';
+import {
+  AmplifyAuthContainer,
+  AmplifyAuthenticator,
+  AmplifyForgotPassword,
+  AmplifySignIn
+} from '@aws-amplify/ui-react';
+import {
+  AuthState,
+  onAuthUIStateChange
+} from '@aws-amplify/ui-components';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
+
 const App = () => {
+  const [authState, setAuthState] = useState();
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+
   const routing = useRoutes(routes);
 
-  return (
+  return authState === AuthState.SignedIn && user ? (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
       {routing}
     </ThemeProvider>
+  ) : (
+    <AmplifyAuthContainer>
+      <AmplifyAuthenticator usernameAlias="email">
+        <AmplifySignIn
+          slot="sign-in"
+          usernameAlias="email"
+          headerText="MyLabs - Identification"
+          hideSignUp="True"
+          forgotPasswordText="Mot de passe oublié"
+          submitButtonText="Connexion"
+          formFields={[
+            {
+              type: 'email',
+              label: 'Email',
+              placeholder: 'Entrer votre email',
+              inputProps: { required: true, autocomplete: 'username' },
+            },
+            {
+              type: 'password',
+              label: 'Mot de passe',
+              placeholder: 'Entrer votre mot de passe',
+              inputProps: { required: true, autocomplete: 'new-password' },
+            }
+          ]}
+        />
+        <AmplifyForgotPassword
+          slot="forgot-password"
+          headerText="Mylabs - Reset du mot de passe"
+          sendButtonText="Envoyer un code"
+          formFields={[
+            {
+              type: 'email',
+              label: 'Email',
+              placeholder: 'Entrer votre email',
+              inputProps: { required: true, autocomplete: 'username' },
+            }
+          ]}
+        />
+      </AmplifyAuthenticator>
+    </AmplifyAuthContainer>
   );
 };
 
--- src/components/DashboardNavbar.js
+++ src/components/DashboardNavbar.js
@@ -12,10 +12,24 @@ import {
 import MenuIcon from '@material-ui/icons/Menu';
 import NotificationsIcon from '@material-ui/icons/NotificationsOutlined';
 import InputIcon from '@material-ui/icons/Input';
+
+import { Auth, Hub } from 'aws-amplify';
 import Logo from './Logo';
 
 const DashboardNavbar = ({ onMobileNavOpen, ...rest }) => {
   const [notifications] = useState([]);
+
+  const handleSignOutButtonClick = async () => {
+    try {
+      await Auth.signOut();
+      Hub.dispatch('UI Auth', { // channel must be 'UI Auth'
+        event: 'AuthStateChange', // event must be 'AuthStateChange'
+        message: 'signedout'// message must be 'signedout'
+      });
+    } catch (error) {
+      console.log('error signing out: ', error);
+    }
+  };
 
   return (
     <AppBar
@@ -37,7 +51,7 @@ const DashboardNavbar = ({ onMobileNavOpen, ...rest }) => {
               <NotificationsIcon />
             </Badge>
           </IconButton>
-          <IconButton color="inherit">
+          <IconButton color="inherit" onClick={handleSignOutButtonClick}>
             <InputIcon />
           </IconButton>
         </Hidden>
